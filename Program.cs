c9b4db1c0d27645a86cb370d497a012b6cc94faf
larrymario <larrymario@126.com>
2014-12-01 21:45:25 +0800

    inital test
    I FOUND I CANT USE GITHUB

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpTest
+{
+    class Program
+    {
+        [STAThread]
+
+        static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Hello CSharp!");
+            Console.ReadLine();
+        }
+    }
+}
